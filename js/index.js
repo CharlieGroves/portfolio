a76edb038d5fdc26202e4d76549912007ee1134c
CharlieGroves <62213643+CharlieGroves@users.noreply.github.com>
2020-06-15 17:51:19 +0100

    .

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function sendMessage() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+    console.log(name, email, subject, message);
+}
