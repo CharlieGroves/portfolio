58c49efc79f8b69826c48df21f4fc497f1352641
CharlieGroves <62213643+CharlieGroves@users.noreply.github.com>
2020-06-16 12:59:17 +0100

    .

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 function sendMessage() {
+
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var subject = document.getElementById("subject").value;
     var message = document.getElementById("message").value;
-    console.log(name, email, subject, message);
+
+    if((name != '') && (email != '') && (subject != '') && (message != '')) {
+        console.log(name, email, subject, message);
+
+        document.getElementById('success').innerHTML = 'Successful!';
+    }
 }
